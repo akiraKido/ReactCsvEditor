344d9e6264ac5b2530704e665450c593795f82cf
Akira Kido <achts.y@gmail.com>
2017-09-28 20:18:33 +0900

    removed ref

--- src/components/CsvTable.tsx
+++ src/components/CsvTable.tsx
@@ -1,21 +1,11 @@
 import * as React from "react";
 
-interface CsvTableState { hoge: string; }
+export interface CsvTableProps { csv: string; }
 
-export class CsvTable extends React.Component<undefined, CsvTableState> {
-
-    constructor() {
-        super();
-
-        this.state = { hoge: "" };
-    }
-
-    updateTable(csv:string) {
-        this.setState({hoge: csv});
-    }
+export class CsvTable extends React.Component<CsvTableProps, undefined> {
 
     render() {
-        return <div>{this.state.hoge}</div>
+        return <div>{this.props.csv}</div>
     }
 
 }
